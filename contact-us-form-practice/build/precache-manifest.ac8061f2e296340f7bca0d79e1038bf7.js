6622b4a590b040296c142fd87a0fdd3b27d67fe2
yuanhaomichael <54728835+yuanhaomichael@users.noreply.github.com>
2020-10-08 23:34:48 +0800

    front end for bidding page

---
+++ contact-us-form-practice/build/precache-manifest.ac8061f2e296340f7bca0d79e1038bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64bd64592fef4e99555df8cc42aad72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bad8dc6ebb84f487d803",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "58d4e383ffedfd227b6f",
+    "url": "/static/css/main.7177b589.chunk.css"
+  },
+  {
+    "revision": "bad8dc6ebb84f487d803",
+    "url": "/static/js/2.4852f2cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4852f2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d4e383ffedfd227b6f",
+    "url": "/static/js/main.d73ea85d.chunk.js"
+  },
+  {
+    "revision": "5882c13ce763f3b16ebf",
+    "url": "/static/js/runtime-main.f6cc8eeb.js"
+  }
+]);
