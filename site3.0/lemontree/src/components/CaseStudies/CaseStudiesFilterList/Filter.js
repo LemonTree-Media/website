8a9ff06705a6ce592c00472fd2bd5ff8f38c23db
wu-rymd <wu.raymond@columbia.edu>
2020-10-23 13:11:09 -0400

    filter

--- site3.0/lemontree/src/components/CaseStudies/CaseStudiesFilterList/Filter.js
+++ site3.0/lemontree/src/components/CaseStudies/CaseStudiesFilterList/Filter.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import Select from 'react-select';
 
-let options = [];
-
 function Filter(props) {
+    let options = [];
+    
     for (let i = 0; i < props.options.length; i++) {
         options.push({value: props.options[i], label: props.options[i]});
     }
 
     const [selectedOption, setSelectedOption] = useState(null);
-    
+    console.log(options);
     return (
         <div className="mb-3 mr-3 w-20 d-inline-block">
           <span className="mr-3 fixed-width-100">{props.name}:</span>
